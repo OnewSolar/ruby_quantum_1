74bc06aa7eb3a0b37419c71f3cc84dc332d5226e
unknown <adrianpalacioscarrillo152@gmail.com>
2021-06-02 08:46:38 -0500

    3 valores mayor, menor o igual

---
+++ 3valores.rb
@@ -0,0 +1,38 @@
+
+puts "Ingrese el primer numero:"
+a = gets.to_i
+
+puts "Ingrese el segundo numero:"
+b = gets.to_i
+
+puts "Ingrese el tercer numero:"
+c = gets.to_i
+
+if (a > b and a > c )
+ puts "El mayor es ",a
+
+elsif (b > a and b > c)
+ puts "El mayor es ",b
+
+elsif (c > a and c > b)
+ puts "El mayor es ",c
+
+ elsif
+    puts "No se debe repetir numeros"
+end
+
+
+    if (a < b and a < c)
+
+    puts "El menor es", a
+
+    elsif (b < a and b < c)
+
+    puts "El menor es", b
+
+    elsif (c < a and c < b)
+
+    puts "El menor es", c
+
+
+end 
