5f311b1ec8a64fab48905e9f46b709449d2b2d1b
unknown <adrianpalacioscarrillo152@gmail.com>
2021-06-01 19:39:54 -0500

    Mayor, menor o igual a 5

---
+++ Ejercicio7.rb
@@ -0,0 +1,13 @@
+puts "Ingrese el numero:"
+Numero= gets.chomp.to_i
+
+if (Numero > 5)
+ puts "El numero #{Numero} es mayor  a 5" 
+
+elsif (Numero == 5) 
+ puts "El numero es igual a 5"
+
+elsif (Numero < 5) 
+ puts "El numero #{Numero} es menor a 5"
+end 
+
