bb17ec0a18d98208c15c4ee79b165d3d7bec1a9c
unknown <adrianpalacioscarrillo152@gmail.com>
2021-06-11 14:39:13 -0500

    Suma de par e impar de 100 numeros

---
+++ clase4/tareas/1al100parimpar.rb
@@ -0,0 +1,22 @@
+class Numerosimparesypares
+
+	if Numerosimparesypares = '__main__'
+		 numerospares = gets.to_i
+         numerosimpares = gets.to_i
+         numerosimpares = 0
+ 	     numerospares = 0
+
+for contador in (1..100) do
+
+	puts contador
+
+   if contador%2==0
+			numerospares = numerospares+contador
+		else
+			numerosimpares = numerosimpares+contador
+	puts "La suma de los numeros pares es: #{numerospares}"
+	puts "La suma de los numeros impares es: #{numerosimpares}"
+ end 
+end 
+end 
+end 
