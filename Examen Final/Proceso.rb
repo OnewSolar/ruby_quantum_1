d1dbe9c49c3e8edebd0662522093075402ff1553
unknown <adrianpalacioscarrillo152@gmail.com>
2021-06-25 22:03:45 -0500

    Final

---
+++ Examen Final/Proceso.rb
@@ -0,0 +1,25 @@
+class Proceso
+  def inicio(string)
+    @string = string 
+  end
+  def retroceso(string)
+    string = string.chars
+    string.each do |n|
+      if number != "#"
+        puts number
+   end
+e1 = "abc#d##c"
+e2 = "abc##d######"
+e3 = "#######"
+e4 = ""
+
+retroceso(e1)
+puts "\n"
+retroceso(e2)
+puts "\n"
+retroceso(e3)
+puts "\n"
+retroceso(e4)
+ end 
+ end 
+end 
